a5655acd0bff62d7ba6b9b12696817f9f17362ae
XiaorenY <sean950111@gmail.com>
2022-05-18 16:34:21 -0700

    feat: token balance of (not working)

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = logOut;
 document.getElementById("btn-balanceof").onclick = balanceOf;
 document.getElementById("btn-mint").onclick = mint;
+document.getElementById("btn-token-balanceof").onclick = balanceOfToken;
 document.getElementById("btn-vote-yes").onclick = voteYes;
 document.getElementById("btn-vote-no").onclick = voteNo;
 
@@ -88,6 +89,40 @@ async function mint() {
   console.log('DISABLED', Moralis.isWeb3Enabled());
   document.getElementById('mint-nft').innerHTML = "Mint NFT from address: " + result.to;
 }
+
+async function balanceOfToken() {
+  const user = Moralis.User.current();
+  console.log(user);
+
+  // const contractAddress = '0x150b077aFF5D0cC64A42e4260A5F52cC9a863a2d';
+  // const contractAddress = '0x0fb19e83d740af9d7642a17e8e3fe2977f3fbf96';
+  // const contractAddress = '0x486c4ceaea947a67523ac44229dbb9dca7c53282';
+  const contractAddress = '0xe914863a1ef30ff526bdf03d9d98787bd3e34895';
+  const abiJson = await fetch('./TokenERC20.json').then(response => {
+    return response.json();
+  });
+  
+  const contractAbi = abiJson['abi'];
+  console.log(contractAbi);
+
+  const readOptions = {
+    contractAddress: contractAddress,
+    functionName: "balanceOf",
+    abi: contractAbi,
+    params: { owner: user.get('ethAddress') },
+  };
+  await Moralis.enableWeb3();
+  console.log('ENABLEd', Moralis.isWeb3Enabled());
+
+  const result = await Moralis.executeFunction(readOptions);
+  console.log(result);
+  await Moralis.deactivateWeb3();
+  console.log('DISABLED', Moralis.isWeb3Enabled());
+
+  document.getElementById('balanceof').innerHTML = result;
+
+}
+
 
 function voteYes() {
   console.log("You vote yes");
